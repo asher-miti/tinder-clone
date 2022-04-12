ee31320f089a5f2c9fdf44f54647086c3a7c976f
asher-miti <asher_3@hotmail.co.uk>
2022-04-12 23:35:45 +0100

    updating loginscreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,13 @@
 import React, { useLayoutEffect } from 'react';
-import { View, Text, Button, ImageBackground } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 import useAuth from '../hooks/useAuth';
+import tw from 'tailwind-rn';
 
 const LoginScreen = ({ navigation }) => {
   const { signInWithGoogle, loading } = useAuth();
@@ -12,11 +19,24 @@ const LoginScreen = ({ navigation }) => {
   });
 
   return (
-    <View>
+    <View style={tw('flex-1')}>
       <ImageBackground
+        resizeMode='cover'
+        style={tw('flex-1')}
         source={{
           uri: 'https://tinder.com/static/tinder.png',
-        }}></ImageBackground>
+        }}>
+        <TouchableOpacity
+          style={[
+            tw('absolute bottom-40 w-52 bg-white p-4 rounded-2xl'),
+            { marginHorizontal: '25%' },
+          ]}
+          onPress={signInWithGoogle}>
+          <Text style={tw('font-semibold text-center')}>
+            Sign in & get swiping
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
